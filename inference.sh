5a90843cfeb092760454e191d38953ad8769550b
jaminjeong <jmjeong@micube.co.kr>
2020-04-19 23:23:08 +0900

    change a sample image

--- inference.sh
+++ inference.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 python inference.py \
-    -i ~/.keras/datasets/flower_photos/daisy/8709110478_60d12efcd4_n.jpg \
+    -i ~/.keras/datasets/flower_photos/daisy/10140303196_b88d3d6cec.jpg \
     -c ./training_checkpoints/ckpt-8 \
